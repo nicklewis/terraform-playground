4f81eefcf02799dd239ca649ea315590a34268ce
Nick Lewis <nick@puppetlabs.com>
2019-01-24 14:12:12 -0800

    Create an instance

--- example.tf
+++ example.tf
@@ -4,3 +4,18 @@ provider "google" {
   region      = "us-west1"
 }
 
+resource "google_compute_instance" "example" {
+  name = "nlew-test-instance"
+  machine_type = "f1-micro"
+  zone = "us-west1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
